0e66221155ca500d5ad41ce4df881ecad7850bfb
tran109 <tran109@sheridancollege.ca>
2020-01-23 09:27:36 -0500

    First commit of the project

---
+++ src/gitexampleweek3/GitExampleWeek3.java
@@ -0,0 +1,17 @@
+package gitexampleweek3;
+
+/**
+ * This class is to review the Git concept from last week
+ *
+ * @author Si Tran
+ */
+public class GitExampleWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    }
+
+}
